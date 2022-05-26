4530b3393bdbcd03c43ca7c2fabc7e4d6c721133
tolgahandemir <70445175+tolgahandemir@users.noreply.github.com>
2022-05-26 23:35:26 +0300

    Changes are made in the code to fix styling and turkish characters

--- atm.py
+++ atm.py
@@ -1,37 +1,45 @@
+
+sys_password = "1234"
+balance = 1000
+money_withdrawal_operation_id = 10
+money_deposit_operation_id = 2
+balance_check_operation_id = 3
+exit_program_operation_id = 4
+
 print("""
 **********************************
 1-Para çekme
 2-Para yatırma
-3-Bakiye sorgulma
+3-balance sorgulma
 4-Programdan çıkma
 """)
-sys_şifre="1238"
-Bakiye=1000
 while True:
-    şifre=input("Şifrenizi giriniz:")
-    if(şifre!=sys_şifre):
-        print("şifreniz yanlış tekrar deneyiniz")
-    elif(şifre==sys_şifre):
-        print("şifreniz doğru yapmak istediğiniz işlemi seçiniz..")
+    password = input("passwordnizi giriniz:")
+    if(password != sys_password):
+        print("passwordniz yanlış tekrar deneyiniz")
+    elif(password == sys_password):
+        print("passwordniz doğru yapmak istediğiniz operationi seçiniz..")
         while True:
-            işlem = int(input("İşleminizi seçiniz..."))
-            if (işlem == 1):
+            operation = int(input("operationinizi seçiniz..."))
+            if (operation == money_withdrawal_operation_id):
 
-                para_çekme = int(input("Çekmek istediğiniz miktari giriniz : "))
-                if(Bakiye-para_çekme<0):
-                    print("Yeterli bakiyeniz yok")
+                money_withdrawal = int(input("Çekmek istediğiniz miktari giriniz : "))
+                # noinspection PyRedundantParentheses
+                if(balance - money_withdrawal < 0):
+                    print("Yeterli balanceniz yok")
                     continue
-                Bakiye-=para_çekme
-                print("Yeni bakiyeniz",Bakiye)
-            elif(işlem==2):
-                para_yatırma=int(input("Yatıracağınız miktarı yazınız : "))
-                Bakiye+=para_yatırma
-                print("Yeni bakiyeniz",Bakiye)
-            elif(işlem==3):
-                print("Bakiyeniz:",Bakiye)
+                balance -= money_withdrawal
+                print("Yeni balanceniz", balance)
+            elif(operation == money_deposit_operation_id):
+                deposit_money = int(input("Yatıracağınız miktarı yazınız : "))
+                balance += deposit_money
+                print("Hesabınıza şu miktarda para yatırılmıştır : ", deposit_money)
+                print("Yeni balanceniz", balance)
+            elif(operation == balance_check_operation_id):
+                print("balanceniz:", balance)
 
-            elif(işlem==4):
+            elif(operation == exit_program_operation_id):
                 print("Hoşçakalın.. :)")
                 break
             else:
-                print("Geçersiz işlem Tekrardan deneyiniz")
+                print("Geçersiz operation Tekrardan deneyiniz")
